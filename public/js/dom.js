c64a78ec46a737a87516d6812dca9cb15794d331
Dev <shayan.dev98@gmail.com>
2019-11-09 22:52:43 +0530

    payment-link-issue-resolved

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,14 +1,31 @@
 // let formType = document.querySelector('#formType');
 // document.addEventListener('DOMContentLoaded', function(){
-//   formType.addEventListener('onclick', )
+//   formType.
 // })
+
+let form01
+function myFunction(type) {
+ if (type === "GST Registration") {
+  form01 = type;
+ }
+ else {
+  form01 = null;
+ }
+}
+
 
 function privateFunction(val) {
 
  // if (document.querySelector('#head').innerHTML == "GST Registration") {
- if (val === "private-limited") {
-  document.querySelector('#private').style.display = "block";
-  document.querySelector('#others').style.display = "none";
+ if (document.querySelector('#formType').value === "GST") {
+  if (val === "private-limited") {
+   document.querySelector('#private').style.display = "block";
+   document.querySelector('#others').style.display = "none";
+  }
+  else {
+   document.querySelector('#private').style.display = "none";
+   document.querySelector('#others').style.display = "block";
+  }
  }
 }
 //}
